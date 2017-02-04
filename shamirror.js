c80db053aaee02c54fd4a957413848ba5aec75aa
Rodrigo Vanegas <rvanegas@gmail.com>
2017-02-03 16:02:18 -0800

    crypto and walkdir

--- shamirror.js
+++ shamirror.js
@@ -1,4 +1,20 @@
 const minimist = require('minimist');
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
+
+const walkdir = (dirname, fn) => {
+  fs.readdirSync(dirname).forEach(basename => {
+    if (basename == '.DS_Store') return;
+    const filename = path.join(dirname, basename);
+    fs.statSync(filename).isDirectory() ? walkdir(filename, fn) : fn(filename);
+  });
+};
+
+walkdir('.', filename => {
+  const content = fs.readFileSync(filename);
+  const sha = crypto.createHash('sha256').update(content).digest('hex');
+});
 
 const argv = minimist(process.argv.slice(2));
 if (argv._.length != 2) {
