cd59d11873d55c9433bfd1b4133243d798f7dc39
Rodrigo Vanegas <rvanegas@gmail.com>
2017-02-04 18:27:38 -0800

    added async, walkdir with async per file tasks

--- shamirror.js
+++ shamirror.js
@@ -1,23 +1,37 @@
 const minimist = require('minimist');
 const crypto = require('crypto');
+const async = require('async');
 const path = require('path');
 const fs = require('fs');
 
-const walkdir = (dirname, fn) => {
-  fs.readdirSync(dirname).forEach(basename => {
-    if (basename == '.DS_Store') return;
+const walkdir = (dirname, fn, walkdirDone) => {
+  const entries = fs.readdirSync(dirname);
+  async.each(entries, (basename, eachDone) => {
     const filename = path.join(dirname, basename);
-    fs.statSync(filename).isDirectory() ? walkdir(filename, fn) : fn(filename);
+    fs.statSync(filename).isDirectory() ?
+      walkdir(filename, fn, eachDone) :
+      fn(filename, eachDone);
+  }, (err) => {
+    walkdirDone()
   });
 };
-
-walkdir('.', filename => {
-  const content = fs.readFileSync(filename);
-  const sha = crypto.createHash('sha256').update(content).digest('hex');
-});
 
 const argv = minimist(process.argv.slice(2));
 if (argv._.length != 2) {
   console.log('usage: <orig> <mirror>');
   process.exit(1);
 }
+
+const [origDir, mirrorDir] = argv._;
+
+const printLine = (filename, done) => {
+  setTimeout(() => {
+    console.log(filename);
+    done();
+  }, Math.random() * 10000);
+//   const content = fs.readFileSync(filename);
+//   const sha = crypto.createHash('sha256').update(content).digest('hex');
+};
+
+walkdir(origDir, printLine, () => console.log('done orig'));
+walkdir(mirrorDir, printLine, () => console.log('done mirror'));
