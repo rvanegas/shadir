9db6e761c519bb31512de916d1f2cf8046feea04
Rodrigo Vanegas <rvanegas@gmail.com>
2017-02-06 12:15:02 -0800

    write shadir files, including for dirs

--- shamirror.js
+++ shamirror.js
@@ -3,20 +3,22 @@ const crypto = require('crypto');
 const async = require('async');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
+
+const ignoreFiles = ['.shadir', '.DS_Store'];
 
 const walkdir = (dirname, fileFunc, dirFunc, walkdirDone) => {
   const walk = (dirname, walkDone) => {
-    const entries = fs.readdirSync(dirname);
+    const entries = _.difference(fs.readdirSync(dirname), ignoreFiles);
     const iteratee = (basename, eachDone) => {
       const filename = path.join(dirname, basename);
       fs.statSync(filename).isDirectory() ?
-        walkdirFunc(filename, eachDone) :
+        walk(filename, value => eachDone(null, value)) :
         fileFunc(filename, eachDone);
     };
-    async.map(entries, iteratee, (err, values) => walkDone(values.join('')));
+    async.map(entries, iteratee, (err, values) => dirFunc(dirname, entries, values, walkDone));
   };
-  const walkdirFunc = (name, done) => walk(name, (values) => dirFunc(name, values, done));
-  walkdirFunc(dirname, walkdirDone);
+  walk(dirname, value => walkdirDone(null, value));
 };
 
 const queue = async.queue((task, done) => task(done), 4);
@@ -27,33 +29,29 @@ const handleFile = (filename, done) => {
     const input = fs.createReadStream(filename);
     input.on('readable', () => {
       const data = input.read();
-      if (data) {
-        hash.update(data);
-      }
-      else {
-        const sha = hash.digest('hex');
-        const log = `${sha} ${filename}\n`;
-        taskDone(null, log);
-      }
+      data ? hash.update(data) : taskDone(null, hash.digest('hex'));
     });
   };
   queue.push(task, done);
 };
 
-const handleDir = (filename, shas, done) => {
-  const log = `${filename}\n${shas}`;
-  done(null, log);
+const handleDir = (dirname, entries, values, done) => {
+  const zipped = _.zip(values, entries);
+  const sorted = _.sortBy(zipped, val => val[1]);
+  const content = sorted.map(pair => pair.join(' ') + '\n').join('');
+  const shadirFilename = path.join(dirname, '.shadir');
+  fs.writeFileSync(shadirFilename, content);
+  handleFile(shadirFilename, (err, value) => done(value));
 };
 
-const argv = minimist(process.argv.slice(2));
-if (argv._.length != 2) {
-  console.log('usage: <orig> <mirror>');
+const argv = minimist(process.argv.slice(2), {boolean: ['s']});
+if (argv._.length != 1) {
+  console.log('usage: shadir [-s] <dir>', argv);
   process.exit(1);
 }
 
 const [origDir, mirrorDir] = argv._;
 
-walkdir(origDir, handleFile, handleDir, (err, shas) => {
+walkdir(origDir, handleFile, handleDir, (err) => {
   console.log('done orig');
-  console.log(shas);
 });
